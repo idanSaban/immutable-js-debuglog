8497efed9395939204f99539784e271693c0dae9
IlannOh <60567814+IlannOh@users.noreply.github.com>
2020-12-01 10:40:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,19 @@ module.exports = function () {
             .map(arg => (arg && arg.toJS && typeof arg.toJS == 'function') ? arg.toJS() : arg);
         console.log(...mappedArguments);
     };
+    
+    function unJSDeep(variable) {
+        const isObject = variable && typeof variable === 'object';
+        const isArray = isObject && Array.isArray(variable);
+        if (isObject) {
+            variable = !isArray && typeof variable.toJS === 'function' ? variable.toJS() : variable;
+            variable = Object.entries(variable).reduce((acc, [subVarKey, subVarVal]) => { acc[subVarKey] = unJSDeep(subVarVal); return acc; }, isArray ? [] : {});
+        }
+
+        return variable;
+    }
+
+    console.deepDebugLog = function () {
+        console.log(...Object.values(arguments || {}).map(unJSDeep));
+    };
 }();
